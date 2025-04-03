dbd0fd291d520be2bbbf79a703cbcc8bd16e636b
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2025-04-03 10:06:42 +0200

    migrated to nx and pnpm

--- src/graphql/gql-sync.service.ts
+++ src/graphql/gql-sync.service.ts
@@ -1,24 +1,32 @@
 import { inject, Injectable } from '@angular/core';
-import { Apollo } from 'apollo-angular';
+import { Apollo, gql } from 'apollo-angular';
 import { map, Observable } from 'rxjs';
-import gql from 'graphql-tag';
-
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export abstract class GqlSyncService<T> {
   protected apollo: Apollo = inject(Apollo);
 
   fetchData(query: string): Observable<T[]> {
-    return this.apollo.watchQuery({ query: gql`${query}` })
+    return this.apollo
+      .watchQuery({
+        query: gql`
+          ${query}
+        `,
+      })
       .valueChanges.pipe(map((result: any) => result.data));
   }
 
-  pushData(mutation: string, variables: Record<string, unknown>): Observable<any> {
+  pushData(
+    mutation: string,
+    variables: Record<string, unknown>
+  ): Observable<any> {
     return this.apollo.mutate({
-      mutation: gql`${mutation}`,
-      variables
+      mutation: gql`
+        ${mutation}
+      `,
+      variables,
     });
   }
 }
